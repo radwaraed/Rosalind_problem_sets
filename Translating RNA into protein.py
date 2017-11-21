0bc8f8aa66ae79711784da9900e58658b4b56fca
Radwa Raed <29765113+radwaraed@users.noreply.github.com>
2017-11-21 12:18:21 -0500

    Update Translating RNA into protein.py

--- Translating RNA into protein.py
+++ Translating RNA into protein.py
@@ -1,11 +1,11 @@
-#Problem
+"""Problem
 The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
 
 The RNA codon table dictates the details regarding the encoding of specific codons into the amino acid alphabet.
 
 Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
 
-Return: The protein string encoded by s.
+Return: The protein string encoded by s."""
 
 #Solution
 table={"UUU": "F", "UUC":"F", "UUA":"L", "UUG":"L", "CUU":"L", "CUC":"L","CUA":"L","CUG":"L","AUU":"I","AUC":"I","AUA":"I","AUG":"M","GUU":"V","GUC":"V","GUA":"V","GUG":"V","UCU":"S","UCC":"S","UCA":"S","UCG":"S","CCU":"P","CCC":"P","CCA":"P","CCG":"P","ACU":"T","ACC":"T","ACG":"T","ACA":"T","GCU":"A","GCC":"A","GCA":"A","GCG":"A","UAU":"Y","UAC":"Y","UAA":"**","UAG":"**","CAU":"H","CAC":"H","CAA":"Q","CAG":"Q", "AAU":"N","AAC":"N","AAA":"K","AAG":"K","GAU":"D","GAC":"D","GAA":"E","GAG":"E", "UGU":"C","UGC":"C","UGA":"**","UGG":"W", "CGU":"R","CGC":"R","CGA":"R","CGG":"R", "AGU":"S", "AGC":"S","AGA":"R","AGG":"R","GGU":"G","GGC":"G","GGA":"G","GGG":"G"}
