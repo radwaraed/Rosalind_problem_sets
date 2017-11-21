15663c0960af33573c3883788afe4670f5278434
Radwa Raed <29765113+radwaraed@users.noreply.github.com>
2017-11-21 12:19:21 -0500

    Update and rename Computing GC content to Computing GC content.py

--- Computing GC content.py
+++ Computing GC content.py
@@ -1,4 +1,4 @@
-#Problem
+"""Problem
 
 The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
 
@@ -8,7 +8,7 @@ In Rosalind's implementation, a string in FASTA format will be labeled by the ID
 
 Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 
-Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
+Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below."""
 
 
 #Solution
